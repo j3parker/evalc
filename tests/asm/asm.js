93c7dc9d9f6173c167c8286fe634e504eee02550
Marc Burns <m4burns@uwaterloo.ca>
2012-04-21 16:46:47 -0400

    Assembler seems to work (untested).

--- tests/asm/asm.js
+++ tests/asm/asm.js
@@ -1,5 +1,5 @@
 function asmtomips(asm) {
-	var s = asm.replace(":",":\n").split("\n").map(
+	var s = asm.replace(/:/g,":\n").split("\n").map(
 			function(x) {
 				return x.replace(/ *; .*$/, "");
 			  }
@@ -8,7 +8,7 @@ function asmtomips(asm) {
 				return (null == /^[ \t]*$/.exec(x));
 			}
 		);
-
+	console.log(s);
 	var type0R = 0
 	    type1R = 1,
 	    type2R = 2,
@@ -18,7 +18,7 @@ function asmtomips(asm) {
 	    typeB = 6,
 	    typeP = 7,
 	    typeL = 8;
-	var rs =
+	var rxs =
 		[
 			// 0R
 			/^[ \t]*([a-z]+)$/,
@@ -40,7 +40,7 @@ function asmtomips(asm) {
 			/^[ \t]*([a-z0-9]+):$/
 		];
 
-	var matches = s.map(function(x) { return rs.map(function(y) { return y.exec(x); }); }).map(
+	var matches = s.map(function(x) { return rxs.map(function(y) { return y.exec(x); }); }).map(
 		function(v) {
 			for(var i=0; i<v.length; i++) {
 				if(v[i] != null) {
@@ -52,6 +52,23 @@ function asmtomips(asm) {
 			}
 			return null;
 		});
+
+	var labels = new Array();
+	var pc = 0;
+	for(idx in matches) {
+		line = matches[idx];
+		if(line[0] == typeL) {
+			if(labels[line[1]] != undefined) {
+				throw "Label '" + line[1] + "' twice defined.";
+			}
+			labels[line[1]] = pc;
+		} else {
+			pc += 4;
+		}
+	}
+	for(idx in labels) {
+		console.log(idx + ":" + labels[idx]);
+	}
 
 	function assert_type(i, t) {
 		if(i[0] != t) {
@@ -85,9 +102,9 @@ function asmtomips(asm) {
 	function asm_i(op, im) {
 		switch(arguments.length) {
 		case 3:
-			return ((op<<26)&0xFC000000) | rs(arguments[2]);
+			return ((op<<26)&0xFC000000) | (im&0x0000FFFF) | rs(arguments[2]);
 		case 4:
-			return ((op<<26)&0xFC000000) | rs(arguments[2]) | rt(arguments[3]);
+			return ((op<<26)&0xFC000000) | (im&0x0000FFFF) | rs(arguments[2]) | rt(arguments[3]);
 		}
 	}
 
@@ -95,239 +112,262 @@ function asmtomips(asm) {
 		return ((op<<26)&0xFC000000) | (im&0x03FFFFFF);
 	}
 
-	var output = [];
-	console.log(matches);
+	function dai(im) {
+		if(!isNaN(im)) {
+			return im;
+		} else {
+			if(labels[im] == undefined) {
+				throw "Label undefined: " + im + ".";
+			}
+			return labels[im];
+		}
+	}
+
+	function dri(im, pc) {
+		var a = dai(im);
+		return (pc - a)/4;
+	}
+
+	var output = new Array();
 	for(idx in matches) {
+		cpc = 0;
 		inst = matches[idx];
-		console.log(inst[1]);
+
 		if(inst == null) {
 			throw "Garbage in input.";
 		}
+
+		if(inst[0] == typeL) {
+			continue;
+		}
+
 		switch(inst[1]) {
 
 		// R type:
 		case "add":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opADD, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fADD, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "addu":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opADDU, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fADDU, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "and":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opAND, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fAND, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "break":
 			assert_type(inst, type0R);
-			output = output.append(asm_r(opBREAK));
+			output.push(asm_r(opBREAK));
 			break;
 		case "div":
 			assert_type(inst, type2R);
-			output = output.append(asm_r(opDIV, 0, +inst[2], +inst[3]));
+			output.push(asm_r(fDIV, 0, +inst[2], +inst[3]));
 			break;
 		case "divu":
 			assert_type(inst, type2R);
-			output = output.append(asm_r(opDIVU, 0, +inst[2], +inst[3]));
+			output.push(asm_r(fDIVU, 0, +inst[2], +inst[3]));
 			break;
 		case "jalr":
 			assert_type(inst, type2R);
-			output = output.append(asm_r(opJALR, 0, +inst[2], +inst[3]));
+			output.push(asm_r(fJALR, 0, +inst[2], +inst[3]));
 			break;
 		case "jr":
 			assert_type(inst, type1R);
-			output = output.append(asm_r(opJR, 0, +inst[2]));
+			output.push(asm_r(fJR, 0, +inst[2]));
 			break;
 		case "mfhi":
 			assert_type(inst, type1R);
-			output = output.append(asm_r(opMFHI, +inst[2]));
+			output.push(asm_r(fMFHI, +inst[2]));
 			break;
 		case "mfhi":
 			assert_type(inst, type1R);
-			output = output.append(asm_r(opMFLO, +inst[2]));
+			output.push(asm_r(fMFLO, +inst[2]));
 			break;
 		case "mthi":
 			assert_type(inst, type1R);
-			output = output.append(asm_r(opMTHI, 0, +inst[2]));
+			output.push(asm_r(fMTHI, 0, +inst[2]));
 			break;
 		case "mtlo":
 			assert_type(inst, type1R);
-			output = output.append(asm_r(opMTLO, 0, +inst[2]));
+			output.push(asm_r(fMTLO, 0, +inst[2]));
 			break;
 		case "mult":
 			assert_type(inst, type2R);
-			output = output.append(asm_r(opMULT, 0, +inst[2], +inst[3]));
+			output.push(asm_r(fMULT, 0, +inst[2], +inst[3]));
 			break;
 		case "multu":
 			assert_type(inst, type2R);
-			output = output.append(asm_r(opMULTU, 0, +inst[2], +inst[3]));
+			output.push(asm_r(fMULTU, 0, +inst[2], +inst[3]));
 			break;
 		case "nor":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opNOR, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fNOR, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "or":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opOR, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fOR, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "sll":
 			//TODO: cocks
 			break;
 		case "sllv":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSLLV, +inst[2], +inst[4], +inst[3]));
+			output.push(asm_r(fSLLV, +inst[2], +inst[4], +inst[3]));
 			break;
 		case "slt":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSLT, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fSLT, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "sltu":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSLTU, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fSLTU, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "sra":
 			//TODO: cocks
 			break;
 		case "srav":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSRAV, +inst[2], +inst[4], +inst[3]));
+			output.push(asm_r(fSRAV, +inst[2], +inst[4], +inst[3]));
 			break;
 		case "srl":
 			//TODO: cocks
 			break;
 		case "srlv":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSRLV, +inst[2], +inst[4], +inst[3]));
+			output.push(asm_r(fSRLV, +inst[2], +inst[4], +inst[3]));
 			break;
 		case "sub":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSUB, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fSUB, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "subu":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opSUBU, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fSUBU, +inst[2], +inst[3], +inst[4]));
 			break;
 		case "syscall":
 			assert_type(inst, type0R);
-			output = output.append(asm_r(opSYSCALL));
+			output.push(asm_r(opSYSCALL));
 			break;
 		case "xor":
 			assert_type(inst, type3R);
-			output = output.append(asm_r(opXOR, +inst[2], +inst[3], +inst[4]));
+			output.push(asm_r(fXOR, +inst[2], +inst[3], +inst[4]));
 			break;
 
 		// I type:
 		case "addi":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opADDI, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opADDI, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "addiu":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opADDIU, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opADDIU, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "andi":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opANDI, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opANDI, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "beq":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opBEQ, +inst[4], +inst[2], +inst[3]));
+			output.push(asm_i(opBEQ, dri(inst[4], cpc), +inst[2], +inst[3]));
 			break;
 		case "bgez":
 			assert_type(inst, type2I);
-			output = output.append(asm_i(opBGEZ, +inst[3], +inst[2], 1));
+			output.push(asm_i(opBGEZ, dri(inst[3], cpc), +inst[2], 1));
 			break;
 		case "bgtz":
 			assert_type(inst, type2I);
-			output = output.append(asm_i(opBGTZ, +inst[3], +inst[2]));
+			output.push(asm_i(opBGTZ, dri(inst[3], cpc), +inst[2]));
 			break;
 		case "blez":
 			assert_type(inst, type2I);
-			output = output.append(asm_i(opBLEZ, +inst[3], +inst[2]));
+			output.push(asm_i(opBLEZ, dri(inst[3], cpc), +inst[2]));
 			break;
 		case "bltz":
 			assert_type(inst, type2I);
-			output = output.append(asm_i(opBLTZ, +inst[3], +inst[2]));
+			output.push(asm_i(opBLTZ, dri(inst[3], cpc), +inst[2]));
 			break;
 		case "bne":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opBNE, +inst[4], +inst[2], +inst[3]));
+			output.push(asm_i(opBNE, dri(inst[4], cpc), +inst[2], +inst[3]));
 			break;
 		case "lb":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLB, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLB, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "lbu":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLBU, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLBU, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "lh":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLH, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLH, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "lhu":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLHU, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLHU, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "lui":
 			assert_type(inst, type2I);
-			output = output.append(asm_i(opLUI, +inst[3], 0, +inst[2]));
+			output.push(asm_i(opLUI, dai(inst[3]), 0, +inst[2]));
 			break;
 		case "lw":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLW, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLW, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "lwc1":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opLWC1, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opLWC1, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "ori":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opORI, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opORI, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "sb":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opSB, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opSB, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "slti":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opSLTI, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opSLTI, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "sltiu":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opSLTIU, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opSLTIU, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 		case "sh":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opSH, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opSH, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "sw":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opSW, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opSW, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "swc1":
 			assert_type(inst, typeP);
-			output = output.append(asm_i(opSWC1, +inst[3], +inst[4], +inst[2]));
+			output.push(asm_i(opSWC1, dai(inst[3]), +inst[4], +inst[2]));
 			break;
 		case "xori":
 			assert_type(inst, typeB);
-			output = output.append(asm_i(opXORI, +inst[4], +inst[3], +inst[2]));
+			output.push(asm_i(opXORI, dai(inst[4]), +inst[3], +inst[2]));
 			break;
 
 		// J type:
 		case "j":
 			assert_type(inst, type1I);
-			output = output.append(asm_j(opJ, +inst[2]));
+			output.push(asm_j(opJ, dai(inst[2])));
 			break;
 		case "jal":
 			assert_type(inst, type1I);
-			output = output.append(asm_j(opJAL, +inst[2]));
+			output.push(asm_j(opJAL, dai(inst[2])));
 			break;
 
 		default:
-			throw "faggot";
+			throw "Unknown opcode: " + inst[1] + ".";
 			break;
 		}
+
+		cpc += 4;
 	}
 
 	return output;	
