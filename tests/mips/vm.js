e307d7ee5f77741b813d6869faa68864df772110
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2012-04-21 16:37:23 -0400

    hooked vm up to ram visualizer

--- tests/mips/vm.js
+++ tests/mips/vm.js
@@ -2,28 +2,29 @@ var RAM_CODE    = 0x01,
     RAM_STACK   = 0x02,
     RAM_HEAP    = 0x04,
     RAM_UNALLOC = 0x08
-    RAM_UNINIT  = 0x10,
-    RAM_UNALLO  = 0x20;
+    RAM_UNINIT  = 0x10;
 
 function make_vm(ramsize) {
   if(typeof ramsize == "undefined" || !isFinite(ramsize)) return null;
-  return {
-    regs: new Uint32Array(32),
-    pc: 0,
-    hi: 0,
-    lo: 0,
-    RAM: new Uint32Array(ramsize),
-    RAMState: new Uint8Array(ramsize),
-    config: {
-      faultOnCodeW:        true,
-      faultOnCodeR:        true,
-      faultOnUnallocR:     true,
-      faultOnUnallocW:     false,
-      faultOnUninitR:      true,
-      faultOnPCEscape:     true,
-      heapAllocOnUnallocW: true,
-    },
+  var vm = new Object();
+  vm.regs = new Uint32Array(32),
+  vm.pc = 0;
+  vm.hi = 0;
+  vm.lo = 0;
+  vm.RAM = new Uint32Array(ramsize);
+  vm.RAMState = new Uint8Array(ramsize);
+  vm.config = new Object();
+  vm.config.faultOnCodeW =        true;
+  vm.config.faultOnCodeR =        true;
+  vm.config.faultOnUnallocR =     true;
+  vm.config.faultOnUnallocW =     false;
+  vm.config.faultOnUninitR =      true;
+  vm.config.faultOnPCEscape =     true;
+  vm.config.heapAllocOnUnallocW = true;
+  for(var i = 0; i < ramsize; i++) {
+    vm.RAMState[i] = RAM_UNALLOC;
   }
+  return vm;
 }
 
 function make_RAM_table(vm, target) {
@@ -35,7 +36,29 @@ function make_RAM_table(vm, target) {
   target.innerHTML = html;
 }
 
+function stateToId(x) {
+  if(x & RAM_CODE)    return "code";
+  if(x & RAM_STACK)   return "stack";
+  if(x & RAM_HEAP)    return "heap";
+  if(x & RAM_UNALLOC) return "unalloc";
+  return '';
+}
+
+function update_ram(vm, x) {
+  var hex = x.toString(16);
+  var row = document.getElementById(hex);
+  var state = vm.RAMState[x];
+  var here = vm.pc == x;
+  row.innerHTML = '<th class="RAM"' + (here? 'id="here"':'') + '>0x' + hex + '</th><td class="RAM" id="' + stateToId(state) + '">' + (((state & RAM_UNINIT) || (state & RAM_UNALLOC)) ? '' : vm.RAM[x]) + '</td>';
+}
+
+function program_ram(vm, loc, op) {
+  vm.RAM[loc] = op;
+  vm.RAMState[loc] = RAM_CODE;
+}
+
 function step(vm) {
+  var oldpc = vm.pc;
   function rs(x)     { return x&0x3E00000>>>21; }
   function rt(x)     { return x&0x1F0000>>>16; }
   function rd(x)     { return x&0xF800>>>11; }
@@ -54,7 +77,8 @@ function step(vm) {
             message: "Attempted to load instruction from unallocated memory.",
             fatal: true,
     };
-  
+  var data, loc;
+  var write_mem = false;
   instr = vm.RAM[vm.pc];
   switch(opcode(instr)) {
     case opARITH:
@@ -112,14 +136,44 @@ function step(vm) {
     case opSLTI:
     case opSLTIU:
     case opSW:
+      var offset = imm(instr);
+      if(offset%4 != 0) throw {
+                          PC: vm.pc,
+                          message: "Unaligned word store.",
+                          fatal: true,
+                        }
+      loc = vm.regs[rs(instr)] + offset/4;
+      data = vm.regs[rt(instr)];
+      write_mem = true;
+      break;
     case opXORI:
     default:
       throw {
         PC: vm.pc,
-        op: instr,
         message: "Invalid opcode.",
         fatal: true,
       }
   }
   vm.pc += 1;
+  update_ram(vm, oldpc);
+  update_ram(vm, vm.pc);
+  if(write_mem) {
+    if(vm.RAMState[loc] & RAM_UNALLOC && vm.config.faultOnUnallocW)
+      throw {
+        PC: vm.pc,
+        message: "Attempted write to unallocated memory",
+        fatal: true,
+      }
+    if(vm.RAMState[loc] & RAM_CODE && vm.config.faultOnCodeW)
+      throw {
+        PC: vm.pc,
+        message: "Attempted write to protected code location.",
+        fatal: true,
+      }
+    if(vm.RAMState[loc] & RAM_UNALLOC && vm.config.heapAllocOnUnallocW)
+      vm.RAMState[loc] ^= RAM_UNALLOC;
+
+    vm.RAM[loc] = data;
+    update_ram(vm, loc);
+  }
 }
