a39fde8c7deef02baf2860fe1f1aac8880834bbd
Marc Burns <m4burns@uwaterloo.ca>
2012-04-23 16:35:48 -0400

    going insane from typecheck

--- tests/parse/walk.js
+++ tests/parse/walk.js
@@ -18,3 +18,122 @@ function ast_walk(ast, f, initacc) {
 
   return initacc;
 }
+
+var INTEGER = 0;
+
+function assert_typeclass(node, typeclass) {
+  switch(typeclass) {
+    case INTEGER:
+      if(node.type != "int") {
+        throw "Type mismatch: Got "+node.type+", expected integer.";
+      }
+      break;
+  }
+}
+
+function assert(cond) {
+  if(!cond) {
+    throw "assert failed.";
+  }
+}
+
+function  well_typed(node, accu) {
+  switch(node.node_type) {
+    case "primary_expression_const":
+      node.type = "int";
+      break;
+    case "primary_expression_id":
+      // TODO: from symbol table
+      break;
+    case "function_call":
+      // TODO: arguments match. to type: return.
+      break;
+    case ".":
+      // TODO: LHS is a struct instance, RHS is a field. to type: RHS type -> typeof expr
+      break;
+    case "post++":
+    case "post--":
+    case "pre++":
+    case "pre--":
+      assert_typeclass(node.target, INTEGER);
+      node.type = node.target.type;
+      break;
+    case "unary&":
+      node.type = type_reference(node.target);
+      break;
+    case "unary*":
+      node.type = type_dereference(node.target);
+      break;
+    case "unary+":
+    case "unary-":
+    case "unary~":
+    case "unary!":
+      assert_typeclass(node.target, INTEGER);
+      node.type = node.target.type;
+      break;
+    case "typecast":
+      // already done in the ast construction. TODO might want to verify cast.
+      break;
+    case "*":
+    case "/":
+    case "%":
+    case ">>":
+    case "<<":
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break; 
+    case "+":
+      //TODO: pointers
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break; 
+    case "-":
+      //TODO: pointers
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break;
+    case "<":
+    case ">":
+    case "<=":
+    case ">=":
+    case "==":
+    case "!=":
+      //TODO: pointers
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break;
+    case "&":
+    case "^":
+    case "|":
+    case "&&":
+    case "||":
+      //TODO: pointers??
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break;
+    case "?":
+      assert(node.targets[1].type == node.targets[2].type);
+      assert_typeclass(node.targets[0], INTEGER);
+      node.type = node.targets[1].type;
+      break;
+    case "=":
+    case "*=":
+    case "/=":
+    case "%=":
+    case "<<=":
+    case ">>=":
+    case "&=":
+    case "^=":
+    case "|=":
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break;
+    case "+=":
+    case "-=":
+      //TODO: pointers
+      node.targets.map(function(x){assert_typeclass(x, INTEGER);});
+      node.type = "int";
+      break;
+  }
+}
+
